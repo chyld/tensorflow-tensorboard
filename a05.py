68da556d4b53fd851253dd37e4d68a6cbcc5b800
Chyld Medford <chyld.medford@gmail.com>
2017-07-08 22:49:16 -0700

    hyperparamter search

--- a05.py
+++ a05.py
@@ -8,37 +8,54 @@ import tensorflow as tf
 X_orig = np.random.randn(2000, 3)
 w_orig = np.array([[0.3, 0.5, 0.1]])
 b_orig = -0.2
-e_orig = np.random.randn(2000, 1) * 0.1
-y_orig = (X_orig @ w_orig.T) + b_orig + e_orig
+e_orig = np.random.randn(1, 2000) * 0.1
+y_orig = (w_orig @ X_orig.T) + b_orig + e_orig
 
-with tf.name_scope('input'):
-    X = tf.placeholder(tf.float32, shape=[None, 3], name='X')
-    y = tf.placeholder(tf.float32, shape=[None, 1], name='y')
+def execute(learning_rate):
+    tf.reset_default_graph()
 
-with tf.name_scope('variables'):
-    weights = tf.Variable([[0, 0, 0]], dtype=tf.float32, name='weights')
-    bias = tf.Variable(0, dtype=tf.float32, name='bias')
+    with tf.name_scope('input'):
+        X = tf.placeholder(tf.float32, shape=[None, 3], name='X')
+        y = tf.placeholder(tf.float32, shape=[1, None], name='y')
 
-with tf.name_scope('prediction'):
-    y_hat = (weights @ tf.transpose(X)) + bias
+    with tf.name_scope('variables'):
+        weights = tf.Variable([[0, 0, 0]], dtype=tf.float32, name='weights')
+        bias = tf.Variable(0, dtype=tf.float32, name='bias')
+        tf.summary.scalar('bias', bias)
 
-with tf.name_scope('loss'):
-    loss = tf.reduce_mean((y - y_hat) ** 2)
-    tf.summary.scalar('loss', loss)
+    with tf.name_scope('prediction'):
+        y_hat = (weights @ tf.transpose(X)) + bias
+        tf.summary.histogram('y_hat', y_hat)
 
-with tf.name_scope('train'):
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.00001).minimize(loss)
+    with tf.name_scope('loss'):
+        loss = tf.reduce_mean((y - y_hat) ** 2)
+        tf.summary.scalar('loss', loss)
 
-writer = tf.summary.FileWriter('a05-rpt')
-summ = tf.summary.merge_all()
+    with tf.name_scope('train'):
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    for i in range(100):
-        sess.run(optimizer, feed_dict={X: X_orig, y: y_orig})
-        s = sess.run(summ, feed_dict={X: X_orig, y: y_orig})
-        writer.add_summary(s, i)
+    writer = tf.summary.FileWriter('a05-rpt/lr' + str(learning_rate))
+    summary = tf.summary.merge_all()
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        writer.add_graph(sess.graph)
+        for i in range(10):
+            sess.run(optimizer, feed_dict={X: X_orig, y: y_orig})
+            s = sess.run(summary, feed_dict={X: X_orig, y: y_orig})
+            writer.add_summary(s, i)
 
-    writer.add_graph(sess.graph)
-    w_final, b_final = sess.run([weights, bias])
-    print('weights:', w_final, 'bias:', b_final)
+            if i == 9:
+                print('--->', i, sess.run([weights, bias]))
+
+### ------------------------------------------------------------------------------------------- ###
+### ------------------------------------------------------------------------------------------- ###
+### ------------------------------------------------------------------------------------------- ###
+
+learning_rates = [0.5, 0.05, 0.005, 0.0005]
+for lr in learning_rates:
+    execute(lr)
+
+### ------------------------------------------------------------------------------------------- ###
+### ------------------------------------------------------------------------------------------- ###
+### ------------------------------------------------------------------------------------------- ###
